48f04edcd5f5cab22db45022cacec455a8874930
diana9barrios <dbarrios@miners.utep.edu>
2024-09-29 22:14:59 -0600

    try again

--- code_4.py
+++ code_4.py
@@ -31,14 +31,16 @@ def get_big_mac_price_by_country(country_code):
 
 def get_the_cheapest_big_mac_price_by_year(year):
     cheapest_year = str(year)
+    # stores first 4 characters of date/ stores in in year_filter
     year_filter = df['date'].str[:4] == cheapest_year
     filtered_df = df[year_filter]
 
     if not filtered_df.empty:
-        cheapest_price = filtered_df.loc[filtered_df['dollar_price'].idxmin()]
-        country = cheapest_price['name']
-        country_code = cheapest_price['iso_a3']
-        price_rounded = round(cheapest_price['dollar_price'], 2)
+        lowest_price = filtered_df.loc[filtered_df['dollar_price'].idxmin()]
+        country = lowest_price['name']
+        country_code = lowest_price['iso_a3']
+        price_rounded = round(lowest_price['dollar_price'], 2)
+
         return f"{country}({country_code}): $ {price_rounded}"
     return None
 
@@ -50,7 +52,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     filtered_df = df[year_filter]
 
     if not filtered_df.empty:
-        # used idxmax to find the min inside a column and .loc to stay in 'bounce'
+        # used idxmax to find the man inside a column and .loc to stay in 'bounce'
         highest_price = filtered_df.loc[filtered_df['dollar_price'].idxmax()]
         country = highest_price['name']
         country_code = highest_price['iso_a3']
